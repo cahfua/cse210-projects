114de86b90aac6a5cd227f441911194bf2720d0d
cahfua <ahfuacelestine@gmail.com>
2025-10-04 02:29:12 -0600

    W05 Learning Activity: Inheritance completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        var simple = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simple.GetSummary());
+        Console.WriteLine();
+
+        var math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        var writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+        Console.WriteLine();
+
+        var assignments = new List<Assignment> { math, writing, simple };
+        foreach (var a in assignments)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
     }
 }
